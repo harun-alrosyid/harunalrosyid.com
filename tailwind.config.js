30e6ce64a18ff56e55396601b4479c84bac739f0
harun-alrosyid <alrosyid07@gmail.com>
2025-09-14 23:12:57 +0700

    feat:  replace class section badge to tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
       xl: '1440px',
     },
     colors: {
-      'color-dark': '#02040a',
+      'dark': '#02040a',
+      'gray-01': '#434343',
+      'gray-02': '#242424',
+      'gray-03': 'rgba(156, 155, 156, 25)',
+      'gray-04': '#3e3e3e',
+    },
+    backgroundColor: {
+      'dark': '#02040a',
       'gray-01': '#434343',
       'gray-02': '#242424',
       'gray-03': 'rgba(156, 155, 156, 25)',
@@ -17,6 +24,9 @@ module.exports = {
     },
     textColor: {
       'white': '#fff',
+    },
+    borderRadius: {
+      '2.5xl': '1.25rem',
     },
     height: {
       "section-regular": "720px",
